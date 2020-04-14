800da1de37edfd9074a568232acc6e9db0865f68
Ravis <Ravis@techfoursolutions.com>
2020-04-14 09:10:00 +0530

    Updated Mpartner IB

---
+++ MpartnerIB_Api/Model/ProductCatalog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LuminousMpartnerIB.MpartnerIB_Api.Model
+{
+    public class ProductCatalog
+    {
+        public int id { get; set; }
+        public string productcatalog_image_url { get; set; }
+        public string productioncatalog_rating { get; set; }
+        public string productioncatalog_name { get; set; }
+        public string keyfeature { get; set; }
+        public string warrenty { get; set; }
+        public string productleveltwo { get; set; }
+        
+        public string attribute_name { get; set; }
+        public List<Technicalspecification> tech_specification { get; set; }
+    }
+    public class Technicalspecification
+    {
+        public string ColumnName;
+        public string Value;
+    }
+}
---
+++ MpartnerIB_Api/Model/UserPermission.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LuminousMpartnerIB.MpartnerIB_Api.Model
+{
+    public class UserPermission
+    {
+        public string ModuleName{get;set;}
+        public string ModuleText { get; set; }
+        public string CustomerType{get;set;}
+        public string Permission { get; set; }
+       // public int MonthValue { get; set; }
+        public string ModuleImage { get; set; }
+    
+    }
+}
