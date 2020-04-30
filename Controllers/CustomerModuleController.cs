c2c1a46f3055419b18d1b23cff25d1ef9efdfdad
tarunrrathi <tarunr@techfoursolutions.com>
2020-04-30 20:36:00 +0530

    code updated on 30-04-2020

---
+++ Controllers/CustomerModuleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Luminous.Models;
+
+namespace Luminous.Controllers
+{
+    public class CustomerModuleController : Controller
+    {
+        // GET: CustomerModule
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ChangeLanguage(string lang)
+        {
+            new LanguageManage().SetLanguage(lang);
+            return RedirectToAction("Luminous", "LuminousHome");
+        }
+    }
+}
